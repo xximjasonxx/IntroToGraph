6bba3411b3be2c1bcb2688ca5e2f1690a0b7c4f8
Jason Farrell <imjason@gmail.com>
2022-06-07 22:56:12 -0400

    added delete all option

--- Application.cs
+++ Application.cs
@@ -37,7 +37,8 @@ namespace GraphDemo
                         .AddOption(6, "Get User Info", () => new GetUserInfoCommand(_querySource))
                         .AddOption(7, "Get Artist Info", () => new GetArtistInfoCommand(_querySource))
                         .AddOption(8, "Count data", () => new CountDataCommand(_querySource))
-                        .AddExitOption(9, "Exit")
+                        .AddOption(9, "Drop all data", () => new DeleteAllCommand(_querySource))
+                        .AddExitOption(10, "Exit")
                         .AddPrompText("Please select an Option: ")
                         .Build();
 
---
+++ Commands/DeleteAllCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GraphDemo.Services;
+
+namespace GraphDemo.Commands
+{
+	public class DeleteAllCommand : ICommand
+	{
+        private readonly IQuerySource _querySource;
+
+		public DeleteAllCommand(IQuerySource querySource)
+		{
+            _querySource = querySource;
+		}
+
+        public async Task ExecuteAsync()
+        {
+            Console.Write("Really delete the graph?: ");
+            var answer = Console.ReadLine()?.ToLower();
+
+            if (answer == "y")
+            {
+                await _querySource.DropAllVertices();
+                Console.WriteLine("Data wipe complete");
+            }
+        }
+    }
+}
+
